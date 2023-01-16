e44b7c4df083289f1ec453e8085cb46cd6b19427
Oleksandr Dehtiarov <retabo.kainrou@gmail.com>
2023-01-16 11:19:26 +0200

    Some update

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,25 @@ import usersFromServer from './api/users';
 import photosFromServer from './api/photos';
 import albumsFromServer from './api/albums';
 
-interface Album {
-  user: string,
-  sexUser: string,
-  id: number,
-  title: string,
-}
+// interface Album {
+//   user: string,
+//   sexUser: string,
+//   id: number,
+//   title: string,
+// }
 
-interface Photo {
-  album: Album,
-  id: number,
-  title: string,
-  url: string,
-}
+// interface Photo {
+//   album: Album,
+//   id: number,
+//   title: string,
+//   url: string,
+// }
 
 export const App: React.FC = () => {
   let userFilter: string;
-  let query: string;
-  const arrAlbumsFilter: boolean[] = [];
 
-  arrAlbumsFilter.fill(false);
+  userFilter = '';
+  const arrFilter: boolean[] = Array(6);
 
   const albums = albumsFromServer.map(album => ({
     user: (usersFromServer.find(user => album.userId === user.id))?.name,
@@ -51,8 +50,35 @@ export const App: React.FC = () => {
     }
   };
 
-  const setActiveUserFilter = (event: React.MouseEvent<HTMLAnchorElement>) => {
+  const setUserFilter = (event: React.MouseEvent<HTMLAnchorElement>) => {
     userFilter = event.currentTarget.text;
+
+    filterArrayOfPhotos();
+  };
+
+  const setOptionFilter = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    switch (event.currentTarget.text) {
+      case 'All':
+        arrFilter[0] = !(arrFilter[0]);
+        break;
+      case 'Album 1':
+        arrFilter[1] = !arrFilter[1];
+        break;
+      case 'Album 2':
+        arrFilter[2] = !arrFilter[2];
+        break;
+      case 'Album 3':
+        arrFilter[3] = !arrFilter[3];
+        break;
+      case 'Album 4':
+        arrFilter[4] = !arrFilter[4];
+        break;
+      case 'Album 5':
+        arrFilter[5] = !arrFilter[5];
+        break;
+      default:
+        break;
+    }
 
     filterArrayOfPhotos();
   };
@@ -69,7 +95,8 @@ export const App: React.FC = () => {
             <p className="panel-tabs has-text-weight-bold">
               <a
                 href="#/"
-                onClick={setActiveUserFilter}
+                className={cn({ 'is-active': userFilter === 'All' })}
+                onClick={setUserFilter}
               >
                 All
               </a>
@@ -77,7 +104,8 @@ export const App: React.FC = () => {
               {usersFromServer.map(user => (
                 <a
                   href="#/"
-                  onClick={setActiveUserFilter}
+                  className={cn({ 'is-active': user.name === userFilter })}
+                  onClick={setUserFilter}
                 >
                   {user.name}
                 </a>
@@ -110,40 +138,52 @@ export const App: React.FC = () => {
             <div className="panel-block is-flex-wrap-wrap">
               <a
                 href="#/"
-                className="button is-success mr-6 is-outlined"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[0] === true })}
+                onClick={setOptionFilter}
               >
                 All
               </a>
 
               <a
-                className="button mr-2 my-1 is-info"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[1] === true })}
                 href="#/"
+                onClick={setOptionFilter}
               >
                 Album 1
               </a>
 
               <a
-                className="button mr-2 my-1"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[2] === true })}
                 href="#/"
+                onClick={setOptionFilter}
               >
                 Album 2
               </a>
 
               <a
-                className="button mr-2 my-1 is-info"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[3] === true })}
                 href="#/"
+                onClick={setOptionFilter}
               >
                 Album 3
               </a>
               <a
-                className="button mr-2 my-1"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[4] === true })}
                 href="#/"
+                onClick={setOptionFilter}
               >
                 Album 4
               </a>
               <a
-                className="button mr-2 my-1"
+                className={cn('button', 'mr-2', 'my-1',
+                  { 'is-info': arrFilter[5] === true })}
                 href="#/"
+                onClick={setOptionFilter}
               >
                 Album 5
               </a>
