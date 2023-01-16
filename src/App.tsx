8e666d5cfa16e7f0d89023a3f6601480b6cafdad
Roman Korchovskyi <korchovskyi.roman@gmail.com>
2023-01-16 10:52:38 +0200

    1-4 tasks solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import photosFromServer from './api/photos';
-// import albumsFromServer from './api/albums';
+import usersFromServer from './api/users';
+import photosFromServer from './api/photos';
+import albumsFromServer from './api/albums';
+
+const allDataFromServer = photosFromServer.map(photo => {
+  const album = albumsFromServer.find(albumId => albumId.id === photo.albumId);
+  const user = usersFromServer.find(userId => userId.id === album?.userId);
+
+  return {
+    id: photo.id,
+    title: photo.title,
+    albumTitle: album?.title,
+    userName: user?.name,
+    sex: user?.sex,
+  };
+});
 
 export const App: React.FC = () => {
+  const [photos] = useState(allDataFromServer);
+  const [query, setQuery] = useState('');
+  const [filterBy, setFilter] = useState('All');
+
+  const photosFilter = () => {
+    const newQuery = query.toLowerCase().trim();
+
+    return photos.filter(photo => {
+      if (filterBy === 'All') {
+        return photo?.title.toLowerCase().includes(newQuery);
+      }
+
+      return photo?.userName === filterBy
+        && photo?.title.toLowerCase().includes(newQuery);
+    });
+  };
+
+  const visiblePhotos = photosFilter();
+
   return (
     <div className="section">
       <div className="container">
@@ -18,28 +52,26 @@ export const App: React.FC = () => {
             <p className="panel-tabs has-text-weight-bold">
               <a
                 href="#/"
+                className={cn(
+                  { 'is-active': filterBy === 'All' },
+                )}
+                onClick={() => setFilter('All')}
               >
                 All
               </a>
 
-              <a
-                href="#/"
-              >
-                User 1
-              </a>
-
-              <a
-                href="#/"
-                className="is-active"
-              >
-                User 2
-              </a>
-
-              <a
-                href="#/"
-              >
-                User 3
-              </a>
+              {usersFromServer.map(user => (
+                <a
+                  key={user.id}
+                  href="#/"
+                  className={cn(
+                    { 'is-active': filterBy === user.name },
+                  )}
+                  onClick={() => setFilter(user.name)}
+                >
+                  {user.name}
+                </a>
+              ))}
             </p>
 
             <div className="panel-block">
@@ -48,7 +80,8 @@ export const App: React.FC = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
-                  value="qwe"
+                  value={query}
+                  onChange={event => setQuery(event.currentTarget.value)}
                 />
 
                 <span className="icon is-left">
@@ -56,11 +89,13 @@ export const App: React.FC = () => {
                 </span>
 
                 <span className="icon is-right">
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    type="button"
-                    className="delete"
-                  />
+                  {query && (
+                    <button
+                      type="button"
+                      className="delete"
+                      onClick={() => setQuery('')}
+                    />
+                  )}
                 </span>
               </p>
             </div>
@@ -111,7 +146,10 @@ export const App: React.FC = () => {
               <a
                 href="#/"
                 className="button is-link is-outlined is-fullwidth"
-
+                onClick={() => {
+                  setQuery('');
+                  setFilter('All');
+                }}
               >
                 Reset all filters
               </a>
@@ -120,80 +158,88 @@ export const App: React.FC = () => {
         </div>
 
         <div className="box table-container">
-          <p data-cy="NoMatchingMessage">
-            No photos matching selected criteria
-          </p>
+          {visiblePhotos.length > 0
+            ? (
+              <table
+                className="table is-striped is-narrow is-fullwidth"
+              >
+                <thead>
+                  <tr>
+                    <th>
+                      <span className="is-flex is-flex-wrap-nowrap">
+                        ID
 
-          <table
-            className="table is-striped is-narrow is-fullwidth"
-          >
-            <thead>
-              <tr>
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    ID
-
-                    <a href="#/">
-                      <span className="icon">
-                        <i data-cy="SortIcon" className="fas fa-sort" />
+                        <a href="#/">
+                          <span className="icon">
+                            <i data-cy="SortIcon" className="fas fa-sort" />
+                          </span>
+                        </a>
                       </span>
-                    </a>
-                  </span>
-                </th>
+                    </th>
 
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    Photo name
+                    <th>
+                      <span className="is-flex is-flex-wrap-nowrap">
+                        Photo name
 
-                    <a href="#/">
-                      <span className="icon">
-                        <i className="fas fa-sort-down" />
+                        <a href="#/">
+                          <span className="icon">
+                            <i className="fas fa-sort-down" />
+                          </span>
+                        </a>
                       </span>
-                    </a>
-                  </span>
-                </th>
+                    </th>
 
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    Album name
+                    <th>
+                      <span className="is-flex is-flex-wrap-nowrap">
+                        Album name
 
-                    <a href="#/">
-                      <span className="icon">
-                        <i className="fas fa-sort-up" />
+                        <a href="#/">
+                          <span className="icon">
+                            <i className="fas fa-sort-up" />
+                          </span>
+                        </a>
                       </span>
-                    </a>
-                  </span>
-                </th>
+                    </th>
 
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    User name
+                    <th>
+                      <span className="is-flex is-flex-wrap-nowrap">
+                        User name
 
-                    <a href="#/">
-                      <span className="icon">
-                        <i className="fas fa-sort" />
+                        <a href="#/">
+                          <span className="icon">
+                            <i className="fas fa-sort" />
+                          </span>
+                        </a>
                       </span>
-                    </a>
-                  </span>
-                </th>
-              </tr>
-            </thead>
+                    </th>
+                  </tr>
+                </thead>
 
-            <tbody>
-              <tr>
-                <td className="has-text-weight-bold">
-                  1
-                </td>
-
-                <td>accusamus beatae ad facilis cum similique qui sunt</td>
-                <td>quidem molestiae enim</td>
-
-                <td className="has-text-link">
-                  Max
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                <tbody>
+                  {visiblePhotos.map(photo => {
+                    return (
+                      <tr key={photo.id}>
+                        <td>{photo.id}</td>
+                        <td>{photo.title}</td>
+                        <td>{photo.albumTitle}</td>
+                        <td
+                          className={cn(
+                            { 'has-text-link': photo?.sex === 'm' },
+                            { 'has-text-danger': photo?.sex === 'f' },
+                          )}
+                        >
+                          {photo.userName}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            ) : (
+              <p data-cy="NoMatchingMessage">
+                No photos matching selected criteria
+              </p>
+            )}
         </div>
       </div>
     </div>
