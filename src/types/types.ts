da180253fcfab9dc5b8f5081f2fbda96b4719f89
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-01-16 10:23:54 +0200

    solution

---
+++ src/types/types.ts
@@ -0,0 +1,23 @@
+export interface Photo {
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+}
+
+export interface Album {
+  userId: number,
+  id: number,
+  title: string,
+}
+
+export interface User {
+  id: number;
+  name: string;
+  sex: string;
+}
+
+export interface PreparedPhoto extends Photo {
+  album: Album,
+  user: User,
+}
